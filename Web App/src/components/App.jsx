9fa6f338cb3a637efcf1773dcacefb8f7966ea05
Priyanshu Pattanaik <hi_priyanshu@outlook.com>
2023-04-02 10:45:15 +0530

    Create folder structure for web app

--- Web App/src/components/App.jsx
+++ Web App/src/components/App.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import Footer from "./Footer";
+import Home from "./Home";
+import NavBar from "./NavBar";
 
 function App(){
-	return ;
+	return (
+		<div>
+			<NavBar />
+			<Home />
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
