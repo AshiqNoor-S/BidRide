5f6fc06df039121d433221fc05d53b2c8abff22e
Priyanshu Pattanaik <hi_priyanshu@outlook.com>
2023-04-15 02:11:13 +0530

    Add Download App Section

--- Web App/src/components/BookARide.jsx
+++ Web App/src/components/BookARide.jsx
@@ -3,11 +3,12 @@ import React from "react";
 function BookARide(){
 	return (
 		<div class="book-a-ride">
-			<h1>BookARide</h1>
+			<h1>Book A Ride Now!</h1>
 			<form method="POST" action="/book-ride">
 				<input type="text" className="pickup-location-input" name="pickupLocation" placeholder="Enter Pickup Point" />
 				<input type="text" className="drop-location-input" name="dropLocation" placeholder="Enter Drop Point" />
 			</form>
+			<div id="map"></div>
 		</div>
 		
 	);
--- Web App/src/components/DownloadApp.jsx
+++ Web App/src/components/DownloadApp.jsx
@@ -2,7 +2,26 @@ import React from "react";
 
 function DownloadApp(){
 	return (
-		<h1 class="download-app">Download the App for Better Experience</h1>
+		<div class="download-app">
+			<h1>Download the App for Better Experience</h1>
+			
+			<img src="images/download-app-image.jpg" id="download-app-img" />
+			
+			<a className="download-button"  href="#">
+				<img src="images/download-app-store.png" id="download-button-1" />
+			</a>
+			
+			<a className="download-button"  href="#">
+				<img src="images/download-google-play.png" id="download-button-2" />
+			</a>
+			
+			
+			
+
+			
+			
+		</div>
+		
 	);
 }
 
